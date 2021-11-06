91924ae96086cf13871739f1e5cbe5e8e05eafdf
unknown <tinb93273@student.ctu.edu.vn>
2021-11-06 19:15:31 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+            <a href="/" class="navbar-brand">Ứng dụng Quản lý danh bạ</a>
+            <div v-if="currentUser" class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <router-link to="/contactbook" class="nav-link">
+                        Danh bạ <i class="fas fa-address-book"></i>
+                    </router-link>
+                </li>
+            </div>
+            <div v-if="!currentUser" class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <router-link to="/register" class="nav-link">
+                        Đăng ký
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/login" class="nav-link">
+                        Đăng nhập
+                    </router-link>
+                </li>
+            </div>
+            <div v-if="currentUser" class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <router-link to="/profile" class="nav-link">
+                        {{ currentUser.username }}
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" @click.prevent="logout"> Đăng xuất </a>
+                </li>
+            </div>
+        </nav>
+        <div class="container mt-3">
+            <router-view />
+        </div>
+    </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script> 
+export default { 
+  name: "App", 
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { handle } from "./common/promise";
+createApp(App)
+  .mixin({
+       methods: {
+           handle,
+      }
+  })
+  .mount('#app');
